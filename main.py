034e494a0cf48e16cd789e36b2b1d5c854edaa46
R1T1KK <161676725+R1T1KK@users.noreply.github.com>
2026-06-20 20:22:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,21 @@ print()
 
 for i in range(1,10,2):
   print(i,end=" ")
+
+# Subtract Product Sum
+class Solution:
+    def subtractProductAndSum(self, n: int) -> int:
+        temp = n
+        sum_ = 0
+        product = 1
+
+        while temp>0:
+            r = temp%10
+            temp//=10
+            sum_+=r
+            product *=r
+        
+        return product - sum_
+            
+        
+        
